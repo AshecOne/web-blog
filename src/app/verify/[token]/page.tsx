6eae8a24aa98c060e695b5f1dc9c5898c7884233
AshecOne <haseyong123@gmail.com>
2024-05-02 11:10:04 +0700

    error deploy

--- src/app/verify/[token]/page.tsx
+++ src/app/verify/[token]/page.tsx
@@ -1,5 +1,6 @@
+"use client";
 import React, { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import axios from "axios";
 import { BASE_URL } from "@/utils/helper";
 import { ToastContainer, toast } from "react-toastify";
