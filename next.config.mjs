3f0cafbbcf42e4cad4f93d5334001f11155ad743
AshecOne <haseyong123@gmail.com>
2024-05-29 22:28:33 +0700

    fix error

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    reactStrictMode: true,
-    exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-      const pathMap = {
-        ...defaultPathMap,
-      };
-      delete pathMap['/verify/[token]'];
-      return pathMap;
-    },
-  };
-  
-  export default nextConfig;
+  output: "standalone",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
