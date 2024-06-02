8df12e5a55b987438ddc6be89373a99da80580d8
AshecOne <haseyong123@gmail.com>
2024-06-02 18:42:03 +0700

    test url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import 'dotenv/config';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone", reactStrictMode: true };
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
+};
 
 export default nextConfig;
