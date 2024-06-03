352728b4212774d7a3df817b6492c5193f337a47
AshecOne <haseyong123@gmail.com>
2024-06-03 13:50:04 +0700

    fix error

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,5 +1,6 @@
+"use client";
 import React, { ComponentType } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useAppSelector } from "@/lib/hooks";
 
 const Auth = <P extends object>(WrappedComponent: ComponentType<P>) => {
