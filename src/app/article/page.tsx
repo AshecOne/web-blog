eac69af5b3ad9c452ed1b1b145a2ef621b53be36
AshecOne <haseyong123@gmail.com>
2024-06-08 16:52:56 +0700

    fix error deploy

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,30 +1,12 @@
 import * as React from "react";
-import { useAppSelector } from "@/lib/hooks";
-import { IArticle } from "@/lib/features/articleSlice";
+import ArticleDetailClient from "../../components/ArticleDetailClient";
 
-const ArticleDetail: React.FunctionComponent = () => {
-  const article = useAppSelector((state) => state.articleReducer.selectedArticle);
-
-  if (!article) {
-    return <div>Loading...</div>;
-  }
-
+const ArticleDetailPage: React.FunctionComponent = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-4">{article.title}</h1>
-      <p className="text-gray-500 text-sm mb-3">
-        By {article.author.username} on{" "}
-        {new Date(article.createdAt).toLocaleDateString("id-ID", {
-          weekday: "long",
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}
-      </p>
-      <img src={article.urlImage} alt={article.title} className="w-full h-auto mb-4" />
-      <p className="text-gray-700 text-lg mb-6">{article.description}</p>
+    <div>
+      <ArticleDetailClient />
     </div>
   );
 };
 
-export default ArticleDetail;
+export default ArticleDetailPage;
